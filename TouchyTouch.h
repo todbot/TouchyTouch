df08f1a2357f98984577fc2d1354e120b85c863c
Tod Kurt <todbot@users.noreply.github.com>
2024-02-04 18:19:22 -0800

    first

---
+++ TouchyTouch.h
@@ -0,0 +1,96 @@
+/**
+ * TouchyTouch - Simple capacitive sense touch library that mimics how CircuitPython touchio works
+ *               But also with debounced rose() / fell() events
+ *
+ * 7 Mar 2023 - @todbot / Tod Kurt
+ *
+ * See: https://gist.github.com/todbot/27c34c55d36002c601b2c28ae8f1b8a4
+ * more really: https://github.com/adafruit/circuitpython/blob/main/shared-module/touchio/TouchIn.c
+ *
+ */
+
+#define N_SAMPLES 10         // default is 10 in touchio, 15-20 works better on picotouch?
+#define CHARGE_MICROS 10     // default is 10 in touchio
+#define TIMEOUT_TICKS 10000
+#define OUTPUT_STYLE OUTPUT_12MA  // matches 12mA outputs of CircuitPython
+
+class TouchyTouch
+{
+ public:
+  TouchyTouch() {}
+
+  // set up a particular touch pin, automatically sets threshold and debounce_interval
+  // but those can be changed later for tuning
+  void begin(int apin = -1, uint16_t debounce_millis=10) {
+    pin = apin;
+    recalibrate();
+    debounce_interval = debounce_millis;
+    last_state = false;
+    changed = false;
+  }
+
+  void recalibrate() {
+    const int num_reads = 5;
+    for(int i=0; i<num_reads; i++) {
+      raw_value += rawRead();
+    }
+    raw_value /= num_reads;
+    threshold = (raw_value * 1.05); 
+  }
+
+  // call update() as fast as possible
+  void update() {
+    changed = false;
+    uint32_t now = millis();
+    if( now - last_debounce_millis > debounce_interval ) {
+      last_debounce_millis = now;
+      bool touch_state = isTouched();
+      changed = touch_state != last_state;
+      last_state = touch_state;
+    }
+  }
+
+  // signal changed to true since last update
+  bool rose() {
+    return changed && last_state==true;
+  }
+
+  // signal changed to false since last update
+  bool fell() {
+    return changed && last_state==false;
+  }
+
+  // cause a read to happen, return true if above threshold
+  bool isTouched() {
+    raw_value = rawRead();
+    return (raw_value > threshold);
+  }
+
+  // do the actual touch detection
+  int16_t rawRead() {
+    uint16_t ticks = 0;
+    for (uint16_t i = 0; i < N_SAMPLES; i++) {
+      // set pad to digital output high for 10us to charge it
+      pinMode(pin, OUTPUT_STYLE);
+      digitalWrite(pin, HIGH);
+      delayMicroseconds(CHARGE_MICROS);
+      // set pad back to an input and take some samples
+      pinMode(pin, INPUT);
+      while ( digitalRead(pin) ) {
+        if (ticks >= TIMEOUT_TICKS) {
+          return TIMEOUT_TICKS;
+        }
+        ticks++;
+      }
+    }
+    return ticks;
+  }
+
+  uint32_t last_debounce_millis;
+  uint16_t debounce_interval;
+  bool last_state;
+  bool changed;
+  uint16_t threshold;
+  int pin;
+  uint16_t raw_value;
+};
---
+++ examples/simpletest/simpletest.ino
@@ -0,0 +1,32 @@
+
+#include "TouchyTouch.h"
+
+const int touch_pins[] = {4, 5, 6, };
+const int touch_count = sizeof(touch_pins) / sizeof(int);
+
+TouchyTouch touches[touch_count];
+
+void setup() {
+  Serial.begin(115200);
+  Serial.println("TouchyTouch simpletest");
+
+  pinMode(LED_BUILTIN, OUTPUT);
+
+  // Touch buttons
+  for (int i = 0; i < touch_count; i++) {
+    touches[i].begin( touch_pins[i] );
+  }
+
+}
+
+void loop() {
+  for ( int i = 0; i < touch_count; i++) {
+    touches[i].update();
+
+    if ( touches[i].isTouched() ) {
+      Serial.print("Pin touched ");
+      Serial.println(i);
+    }
+
+    delay(10);
+}
